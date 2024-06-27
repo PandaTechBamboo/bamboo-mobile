bcbe47a78b7a319dad5bf833c3358aae69f1ce6e
brthhule <brennenle@yahoo.com>
2024-06-27 16:32:07 -0400

    Worked on more LoginScreen styling, changed input fonts, want to implement a new flex assignment system based on variables

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -9,9 +9,22 @@ import React, { useState } from 'react';
 
 export default function LoginScreen({navigation}) {
 
+  var gapOne = 100;
+  var gapTwo = 25;
+  var gapThree = 25;
+  var gapFour = 20;
+  var gapFive = 20;
+  var gapSix = 20;
+  var gapSeven = 20;
+  var gapEight = 60;
+
+  const textFontSize = 20;
+
     const [fontsLoaded] = useFonts({
         'IrishGrover-Regular': require('../assets/fonts/IrishGrover-Regular.ttf'),
         'Itim-Regular' : require('../assets/fonts/Itim-Regular.ttf'),
+        'Besley-Regular' : require('../assets/fonts/Besley-Regular.ttf'),
+        'Besley-Italic' : require('../assets/fonts/Besley-Italic.ttf')
       });
 
     const [username, setUsername] = useState('');
@@ -80,18 +93,20 @@ export default function LoginScreen({navigation}) {
         width: '80%',
         alignItems: 'center',
         flex: 100,
+        fontSize: textFontSize,
       },
       input: {
           width: 300,
           borderColor: '#ccc',
           borderWidth: 1,
           borderRadius: 5,
-          fontFamily: 'Itim-Regular',
+          fontFamily: 'Besley-Italic',
+          
           color: '#FFFFFF',
           backgroundColor: '#D9D9D9',
           paddingLeft: 15,
           fontStyle: 'italic',
-          fontSize: 25,
+          fontSize: textFontSize,
           flex: 40,
       },
     })
@@ -101,32 +116,35 @@ export default function LoginScreen({navigation}) {
       return (
         <View style={signInStyles.container}>
           <TouchableOpacity style={signInStyles.button} onPress={() => navigation.navigate('Home')}>
-            <Text style={signInStyles.buttonText}>Sign in</Text>
+            <Text style={signInStyles.buttonText}>Sign In</Text>
           </TouchableOpacity>
         </View>)
     }
 
     const signInStyles = StyleSheet.create({
       button: {
+        justifyContent: 'center',
+        alignItems: 'center',
         flex: 1,
         aspectRatio: 1,
         borderRadius: 100,
+        borderStyle: 'solid',
+        borderWidth: 5,
+        borderColor: '#A3A3A3',
         backgroundColor: '#726969',
         flexDirection: 'column',
       },
       container: {
+        justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'column',
         flex: 100,
-        backgroundColor: '#00FF00',
       },
       buttonText: {
         color: '#FFFFFF',
-        fontSize: 25,
-        flex: 1,
+        fontSize: textFontSize,
         textAlign: 'center',
         fontFamily: 'Itim-Regular',
-        textAlginVertical: 'center',
       },
     })
 
@@ -138,10 +156,10 @@ export default function LoginScreen({navigation}) {
             <Text style={buttonsStyles.text}>Sign Up</Text>
           </TouchableOpacity>
 
-          <View style={{flex: 20}}/>
+          <View style={{flex: gapSeven}}/>
 
           <TouchableOpacity style={buttonsStyles.buttonContainer} onPress={() => navigation.navigate('Home')}>
-            <Text style={buttonsStyles.text}>Continue as a Guest</Text>
+            <Text style={buttonsStyles.text}>Guest Login</Text>
           </TouchableOpacity>
         </View>
       )
@@ -151,8 +169,6 @@ export default function LoginScreen({navigation}) {
     const buttonsStyles = StyleSheet.create({
       container: {
         flex: 100,
-        
-
       },
       buttonContainer: {
         width: 300,
@@ -160,13 +176,12 @@ export default function LoginScreen({navigation}) {
 
         borderColor: '#ccc',
         borderWidth: 1,
-        borderRadius: 5,
+        borderRadius: 50,
         paddingHorizontal: 10,
         fontFamily: 'Itim-Regular',
         backgroundColor: '#726969',
         paddingLeft: 15,
         fontStyle: 'italic',
-        fontSize: 25,
 
         justifyContent: 'center',
       },
@@ -174,7 +189,7 @@ export default function LoginScreen({navigation}) {
         textAlign: 'center',
         color: '#FFFFFF',
         fontFamily: 'Itim-Regular',
-        fontSize: 25,
+        fontSize: textFontSize,
 
       }
     })
@@ -184,41 +199,45 @@ export default function LoginScreen({navigation}) {
     
     return (
       <View style={styles.container}>
-        <View style={{flex: 135}}/>
+        <View style={{flex: gapOne}}/>
         <TitleText/>
 
-        <View style={{flex: 25}}/>
+        <View style={{flex: gapTwo}}/>
         <View style={logoStyles.imageContainer}>
           <ImageViewer theme="panda-image" imageSource={pandaImage}/>
         </View>
 
-        <View style={{flex: 25}}/> 
+        <View style={{flex: gapThree}}/> 
         <View style={inputStyles.inputContainer}>
           <TextInput
               style={inputStyles.input}
               placeholder="Username..."
+              fontSize={textFontSize}
+              placeholderTextColor="#FFFFFF"
               value={username}
               onChangeText={setUsername}
           />
 
-          <View style={{flex: 20}}/>
+          <View style={{flex: gapFour}}/>
 
           <TextInput
             style={inputStyles.input}
             placeholder="Password..."
+            fontSize={textFontSize}
+            placeholderTextColor="#FFFFFF"
             value={password}
             onChangeText={setPassword}
             secureTextEntry
           />
         </View>
 
-        <View style={{flex: 20}}/>
+        <View style={{flex: gapFive}}/>
         <SignInButton/>
 
-        <View style={{flex: 25}}/>
+        <View style={{flex: gapSix}}/>
         <RectangleButtons/>
 
-        <View style={{flex: 60}}/>
+        <View style={{flex: gapEight}}/>
         
         <StatusBar style="auto" />
       </View>
